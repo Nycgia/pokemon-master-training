cc283d5d4d0b38720c499df78a5ad7a0d6758667
Nycgia <pmunozb@protonmail.com>
2020-05-13 23:31:43 -0500

    #1 A simple view has created

---
+++ src/containers/SelectionTraining.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SelectionTraining = () => {
+    return (
+        <h1>Selection Training</h1>
+    );
+}
+
+export default SelectionTraining;
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import SelectionTraining from '../containers/SelectionTraining';
 import '../assets/styles/App.css';
 
 const App = () => {
     return (
         <BrowserRouter>
             <Navbar />
+            <Switch>
+                <Route exact path="/" component={SelectionTraining} />
+            </Switch>
         </BrowserRouter>
     );
 };
