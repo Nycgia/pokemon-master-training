71b08e9837af2bc7aae25d619e687aabdb045515
Nycgia <pmunozb@protonmail.com>
2020-05-14 22:18:29 -0500

    #1 Buttons is added

---
+++ src/components/SelectionButton.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../assets/styles/components/SelectionButton.css';
+
+const SelectionButton = ({id, label, img}) => {
+    return (
+        <div className="selection-button">
+            Lorem ipsum dolor sit amet.
+        </div>
+    );
+};
+
+export default SelectionButton;
