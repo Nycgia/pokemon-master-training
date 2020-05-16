02f10a43dd03b2d1f6f3a5d8ea5ba37f831b873a
Nycgia <pmunozb@protonmail.com>
2020-05-15 23:37:47 -0500

    #1 Label is added

--- src/components/SelectionButton.jsx
+++ src/components/SelectionButton.jsx
@@ -4,7 +4,7 @@ import '../assets/styles/components/SelectionButton.css';
 const SelectionButton = ({id, label, img}) => {
     return (
         <div className="selection-button">
-            <img src={img} alt={label} />
+            <img src={img} alt={label} title={label} />
         </div>
     );
 };
