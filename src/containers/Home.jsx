10145c4d7648699bf9966b97390d4536480329cf
Nycgia <pmunozb@protonmail.com>
2020-05-20 22:00:23 -0500

    #3 Content and style simple is added

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import '../assets/styles/containers/Home.css';
 
 const Home = () => {
     return (
-        <h1>Home</h1>
+        <div className="home">
+            <section>
+                <article><h2>Learning</h2></article>
+                <article><h2>Training</h2></article>
+                <article><h2>News</h2></article>
+            </section>
+            <aside>
+                <div>Ranking</div>
+                <div>Curious</div>
+            </aside>
+        </div>
     );
 }
 
